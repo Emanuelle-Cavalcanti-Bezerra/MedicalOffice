3bbe0bb3a972135cf52a7ebba2186f2047734b34
emanuelle-cavalcanti-bezerra <emanuellebezerra@gmail.com>
2024-04-03 17:29:55 -0300

    configurações para deployment

--- medical_office_manager/medical_office_manager/wsgi.py
+++ medical_office_manager/medical_office_manager/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical_office_manager.settings')
+settings_module = 'medical_office_manager.deployment_settings' if 'WEBSITE_HOSTNAME' in os.environ else 'medical_office_manager.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
