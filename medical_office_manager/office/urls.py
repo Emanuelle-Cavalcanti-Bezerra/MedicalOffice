aed0ef6156f3e8f9d4ba99080c889bce2d77811c
emanuelle-cavalcanti-bezerra <emanuellebezerra@gmail.com>
2024-04-30 23:49:24 -0300

    agendamento de consultas: template, view e url

--- medical_office_manager/office/urls.py
+++ medical_office_manager/office/urls.py
@@ -18,5 +18,7 @@ urlpatterns = [
     path('patient_successfully_deleted/<int:patient_id>/', views.patient_successfully_deleted, name = 'patient_successfully_deleted'),
     path('appointments_list_assistant/', views.appointments_list_assistant, name = 'appointments_list_assistant'),
     path('appointments_list_doctor/', views.appointments_list_doctor, name = 'appointments_list_doctor'),
+    path('schedule_appointment/<str:date_time>/', views.schedule_appointment, name = 'schedule_appointment'),
+    path('appointment_successfully_scheduled/', views.appointment_successfully_scheduled, name='appointment_successfully_scheduled'),
     
 ]
