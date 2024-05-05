074a4ee653f4be01a2cd05d02c4cfa4144a23789
Emanuelle-Cavalcanti-Bezerra <emanuellebezerra@gmail.com>
2024-05-05 19:35:26 -0300

    add cenário de teste em que o usuário não escolhe uma data na listagem de consultas e a listagem do dia atual é exibida por default

--- medical_office_manager/cypress/e2e/SeeAppointmentsPerDateAssistant.cy.js
+++ medical_office_manager/cypress/e2e/SeeAppointmentsPerDateAssistant.cy.js
@@ -1,3 +1,6 @@
+import {getCurrentDate} from './utils.js'
+import {formatDateBR} from './utils.js'
+
 describe('test suite SeeAppointmentsPerDateAssistant', () => {
     it('List of appointments of a chosen date is displayed to assistant', () => {
         // Executar setup de preparação para o teste
@@ -25,5 +28,25 @@ describe('test suite SeeAppointmentsPerDateAssistant', () => {
         cy.contains('Agendar').should('be.visible') 
         cy.contains('Desmarcar').should('be.visible')  
     })
+    it('List of appointments of current date is displayed to assistant. Assistant does not chose a day', () => {
+        // Executar setup de preparação para o teste
+        cy.exec("python scripts/setup_test_see_appointments_per_date_assistant.py")
+        
+        // Acessar página de home do assistente
+        cy.visit('/office/home_assistant/');
+        
+        // Login como usuário do grupo assistentes
+        cy.get('[name="username"]').type('assistente1')
+        cy.get('[name="password"]').type('fds20241')
+        cy.get('#loginButton').click()
+
+        // Clicar na opção "Consultas" na navbar da home
+        cy.get('#appointmentsListAssistant').click()
+        
+        // Verificar se a programação de consultas do dia atual foi renderizada corretamente
+        cy.get("#inputDataConsultas").invoke("val").should("eq", getCurrentDate())
+        cy.contains(`CONSULTAS - ${formatDateBR(getCurrentDate())}`).should('be.visible')
+    })
 
 })
+
--- medical_office_manager/cypress/e2e/SeeAppointmentsPerDateDoctor.cy.js
+++ medical_office_manager/cypress/e2e/SeeAppointmentsPerDateDoctor.cy.js
@@ -1,3 +1,6 @@
+import {getCurrentDate} from './utils.js'
+import {formatDateBR} from './utils.js'
+
 describe('test suite SeeAppointmentsPerDateDoctor', () => {
     it('List of appointments of default date (current day) is displayed to doctor', () => {
             // Executar setup de preparação para o teste
@@ -25,5 +28,24 @@ describe('test suite SeeAppointmentsPerDateDoctor', () => {
             cy.contains('--------------------').should('be.visible') 
             cy.contains('Ver').should('be.visible')  
         })
-
+        it('List of appointments of current date is displayed to doctor. Doctor does not chose a day', () => {
+            // Executar setup de preparação para o teste
+            cy.exec("python scripts/setup_test_see_appointments_per_date_doctor.py")
+            
+            // Acessar página de home do medico
+            cy.visit('/office/home_doctor/');
+            
+            // Login como usuário do grupo medicos
+            cy.get('[name="username"]').type('medico1')
+            cy.get('[name="password"]').type('fds20241')
+            cy.get('#loginButton').click()
+    
+            // Clicar na opção "Consultas" na navbar da home
+            cy.get('#appointmentsListDoctor').click()
+            
+            // Verificar se a programação de consultas do dia atual foi renderizada corretamente
+            cy.get("#inputDataConsultas").invoke("val").should("eq", getCurrentDate())
+            cy.contains(`CONSULTAS - ${formatDateBR(getCurrentDate())}`).should('be.visible')
+        })
+ 
 })
