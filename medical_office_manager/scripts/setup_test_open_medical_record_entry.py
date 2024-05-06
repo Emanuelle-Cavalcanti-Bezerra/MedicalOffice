cd38194f1ba6ce1a421d59d67944e44bd7b5d9c5
Emanuelle-Cavalcanti-Bezerra <emanuellebezerra@gmail.com>
2024-05-05 22:06:24 -0300

    refatorando os scripts de seeding dos testes

--- medical_office_manager/scripts/setup_test_open_medical_record_entry.py
+++ medical_office_manager/scripts/setup_test_open_medical_record_entry.py
@@ -4,7 +4,10 @@ try:
     
     base_scripts.add_doctor()
        
-    base_scripts.add_appointment('2024-07-17', '08:00:00', 'João Dantas', '2002-07-17', '02367016062', '35351587')
+    base_scripts.add_patient('João Dantas', '2002-07-17', '02367016062', '35351587')
+    
+    base_scripts.add_appointment('2024-07-17', '08:00:00','02367016062')
+
     
     
 except Exception as error:
